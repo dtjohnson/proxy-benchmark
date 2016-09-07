7b7eb7b6378dac12e09b9e51252371675a040221
Dave Johnson <dave@dtjohnson.net>
2016-09-07 19:58:56 -0400

    Created upstream server.

---
+++ upstream/server/index.js
@@ -0,0 +1,41 @@
+"use strict";
+
+const cluster = require('cluster');
+const http = require('http');
+const os = require('os');
+const randomstring = require("randomstring");
+
+process.on('SIGINT', () => process.exit(1));
+process.on('SIGTERM', () => process.exit(1));
+
+if (cluster.isMaster) {
+    const numCPUs = os.cpus().length;
+    for (var i = 0; i < numCPUs; i++) {
+        cluster.fork();
+    }
+
+    return;
+}
+
+console.log("Generating random messages...");
+const msgs = {};
+for (let i = 0; i <= 7; i++) {
+    const size = Math.pow(10, i);
+    msgs[size] = randomstring.generate(size);
+}
+
+console.log("Starting server...");
+http.createServer((req, res) => {
+    const size = req.url.substr(1);
+    if (msgs[size]) {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end(msgs[size]);
+    } else {
+        res.statusCode = 400;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end("Invalid message size");
+    }
+}).listen(8080);
+
+console.log("Upstream listening on port 8080...");
