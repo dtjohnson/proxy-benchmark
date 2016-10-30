6bb3b96c03b645af90bac0584bbd0088c22020b3
Dave Johnson <dave@dtjohnson.net>
2016-10-30 13:11:13 -0400

    Fixed gzip.

--- proxies/haproxy/startup.sh
+++ proxies/haproxy/startup.sh
@@ -2,14 +2,6 @@
 
 : "${UPSTREAM_HOST:?You must set the UPSTREAM_HOST environmental variable}"
 : "${UPSTREAM_PORT:?You must set the UPSTREAM_PORT environmental variable}"
-: "${GZIP:?You must set the GZIP environmental variable}"
-
-COMPRESSION=""
-if [[ "$GZIP" == "on" ]]; then
-    COMPRESSION="compression algo gzip"
-fi
-
-export COMPRESSION
 
 envsubst < /haproxy.cfg.template > /usr/local/etc/haproxy/haproxy.cfg
 
