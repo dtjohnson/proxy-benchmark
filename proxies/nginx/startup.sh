2f3a1dc1650b1d84614cfd8e5d109154284e377c
Dave Johnson <dave@dtjohnson.net>
2016-10-30 12:38:32 -0400

    Added gzip option to HAProxy and Nginx.

--- proxies/nginx/startup.sh
+++ proxies/nginx/startup.sh
@@ -2,6 +2,7 @@
 
 : "${UPSTREAM_HOST:?You must set the UPSTREAM_HOST environmental variable}"
 : "${UPSTREAM_PORT:?You must set the UPSTREAM_PORT environmental variable}"
+: "${GZIP:?You must set the GZIP environmental variable}"
 
 envsubst < /nginx.conf.template > /etc/nginx/nginx.conf
 
