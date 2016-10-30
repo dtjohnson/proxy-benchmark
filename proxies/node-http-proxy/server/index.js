36157e4a648af6072029dda2e4638efc942e5f13
Dave Johnson <dave@dtjohnson.net>
2016-10-30 13:29:48 -0400

    Added gzip and clustering to node-http-proxy.

--- proxies/node-http-proxy/server/index.js
+++ proxies/node-http-proxy/server/index.js
@@ -1,7 +1,11 @@
 "use strict";
 
+const cluster = require('cluster');
 const http = require('http');
+const os = require('os');
 const httpProxy = require('http-proxy');
+const connect = require("connect");
+const compression = require("compression");
 
 process.on('SIGINT', () => process.exit(1));
 process.on('SIGTERM', () => process.exit(1));
@@ -16,10 +20,22 @@ if (!process.env.UPSTREAM_PORT) {
     process.exit(1);
 }
 
-const target = `http://${process.env.UPSTREAM_HOST}:${process.env.UPSTREAM_PORT}`;
-const proxy = httpProxy.createProxy();
-http.createServer((req, res) => {
-    proxy.web(req, res, { target });
-}).listen(80);
+const numCPUs = os.cpus().length;
 
-console.log("node-http-proxy listening on port 80...");
+if (cluster.isMaster) {
+    for (let i = 0; i < numCPUs; i++) {
+        cluster.fork();
+    }
+} else {
+    const target = `http://${process.env.UPSTREAM_HOST}:${process.env.UPSTREAM_PORT}`;
+    const proxy = httpProxy.createProxy();
+    const app = connect();
+    app.use(compression());
+    app.use((req, res) => {
+        proxy.web(req, res, { target });
+    });
+
+    http.createServer(app).listen(80);
+
+    console.log("node-http-proxy listening on port 80...");
+}
