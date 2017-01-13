d9c6d0116184d7f69a45f8e21e72da9f362b8ff2
Dave Johnson <dave@dtjohnson.net>
2017-01-13 08:42:03 -0500

    Made some Node proxy optimizations.

--- proxies/node/server/proxy.js
+++ proxies/node/server/proxy.js
@@ -2,20 +2,25 @@
 
 const http = require('http');
 
-const stripHeaders = [
+const stripHeaders = {};
+[
     // From RFC 2616 section 13.5.1
-    "connection",
-    "keep-alive",
-    "proxy-authenticate",
-    "proxy-authorization",
-    "te",
-    "trailers",
-    "transfer-encoding",
-    "upgrade",
+    "Connection",
+    "Keep-Alive",
+    "Proxy-Authenticate",
+    "Proxy-Authorization",
+    "TE",
+    "Trailers",
+    "Transfer-Encoding",
+    "Upgrade",
 
     // We don't want to trigger upstream HTTPS redirect
-    "upgrade-insecure-requests"
-];
+    "Upgrade-Insecure-Requests"
+].forEach(header => {
+    // Save in object as standard form and lower case. We'll check standard form first and resort to lower case comparison if needed.
+    stripHeaders[header] = true;
+    stripHeaders[header.toLowerCase()] = true;
+});
 
 // N.B. It's OK for the moment to pass along the accept-encoding header as this proxy doesn't
 // modify the content at all.
@@ -31,23 +36,24 @@ module.exports = opts => {
             port: opts.upstreamPort,
             agent
         }, proxyRes => {
-            res.statusCode = proxyRes.statusCode;
+            // res.statusCode = proxyRes.statusCode;
 
             // Strip response headers that shouldn't be send to the client.
             for (let i = 0; i < proxyRes.rawHeaders.length; i += 2) {
                 const name = proxyRes.rawHeaders[i];
-                if (stripHeaders.includes(name.toLowerCase())) continue;
+                if (stripHeaders[name] || stripHeaders[name.toLowerCase()]) continue;
                 const value = proxyRes.rawHeaders[i + 1];
                 res.setHeader(name, value);
             }
 
+            res.writeHead(proxyRes.statusCode);
             proxyRes.pipe(res);
         });
 
         // Strip request headers that shouldn't be sent upstream.
         for (let i = 0; i < req.rawHeaders.length; i += 2) {
             const name = req.rawHeaders[i];
-            if (stripHeaders.includes(name.toLowerCase())) continue;
+            if (stripHeaders[name] || stripHeaders[name.toLowerCase()]) continue;
             const value = req.rawHeaders[i + 1];
             proxyReq.setHeader(name, value);
         }
@@ -56,14 +62,14 @@ module.exports = opts => {
 
         // Handle any proxy request errors by sending a 502.
         proxyReq.on("error", e => {
-            res.statusCode = 502;
+            res.writeHead(502);
             res.end(e.message);
         });
 
         // Handle any request errors by returning a 400.
         req.on("error", e => {
             proxyReq.abort();
-            res.statusCode = 400;
+            res.writeHead(400);
             res.end(e.message);
         });
 
